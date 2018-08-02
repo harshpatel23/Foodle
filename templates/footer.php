72aff254bfa00b806c86d4abc8e4635d957da9f4
Tanay Raul <tanay.raul@somaiya.edu>
2018-08-02 12:22:29 +0530

    ...

--- templates/footer.php
+++ templates/footer.php
@@ -1,11 +1,10 @@
-<footer>
-		<p>	ENTER TAG LINE HERE</p>
-		<a href="	">About us</a>	|
-		<a href="	">facebook</a>	|
-		<a href="	">instagram</a>	|
-		<a href="	">Feedback</a>	|
-		<a href="	">Contact us</a>
-	</footer>
-
+<div class-"footer">
+	<p>	ENTER TAG LINE HERE</p>
+	<a href="	">About us</a>	|
+	<a href="	">facebook</a>	|
+	<a href="	">instagram</a>	|
+	<a href="	">Feedback</a>	|
+	<a href="	">Contact us</a>
+</div>
 </body>
 </html>
