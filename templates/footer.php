7d2df94a4550e485d4686bc5c79e2f39b00c5ee2
Tanay Raul <tanay.raul@somaiya.edu>
2018-08-08 21:51:31 +0530

    footer icons updated.. not finalised

--- templates/footer.php
+++ templates/footer.php
@@ -1,9 +1,9 @@
 <div id="footer" class="footer">
 	
-	<a href="#" class="fa fa-facebook-square"></a>
-	<a href="#" class="fa fa-twitter-square"></a>
-	<a href="#" class="fa fa-google-plus-square"></a>
-	<a href="#" class="fa fa-linkedin-square"></a>
+	<a href="#" class="fa fa-facebook-square" title="Facebook"></a>
+	<a href="#" class="fa fa-twitter-square" title="Twitter"></a>
+	<a href="#" class="fa fa-google-plus-square" title="Google+"></a>
+	<a href="#" class="fa fa-linkedin-square" title="LinkedIn"></a>
 	
 </div>
 </body>
