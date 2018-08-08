daa0f1149e6362e07d937cbab90210ebe7b826b9
Harsh Patel <harsh.patel4@somaiya.edu>
2018-08-08 11:34:27 +0530

    icon hover

--- templates/footer.php
+++ templates/footer.php
@@ -1,9 +1,9 @@
 <div id="footer" class="footer">
 	
-	<a href="#" class="fa fa-facebook"></a>
-	<a href="#" class="fa fa-twitter"></a>
+	<a href="#" class="fa fa-facebook-square"></a>
+	<a href="#" class="fa fa-twitter-square"></a>
 	<a href="#" class="fa fa-instagram"></a>
-	<a href="#" class="fa fa-linkedin"></a>
+	<a href="#" class="fa fa-linkedin-square"></a>
 	
 </div>
 </body>
