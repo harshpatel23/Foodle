c97f5a37645bf3d098e55a675ee6f33e3409435b
Harsh Patel <harsh.patel4@somaiya.edu>
2018-08-10 11:26:39 +0530

    grid added

---
+++ templates/grid.php
@@ -0,0 +1,20 @@
+<style>
+    #container{
+        max-width: 97%;
+        margin:10px auto;
+    }
+</style>
+
+
+
+<div class="container" id="container">
+  <h1>Hello World!</h1>
+  <div class="row">
+    <div class="col-sm-6" style="background-color:yellow;">
+      <p>Lorem ipsum...</p>
+    </div>
+    <div class="col-sm-6" style="background-color:pink;">
+      <p>Sed ut perspiciatis...</p>
+    </div>
+  </div>
+</div>
