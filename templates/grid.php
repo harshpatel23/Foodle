ad3d6755602bcfa6787a196afc3239aecb947b32
Harsh Patel <harsh.patel4@somaiya.edu>
2018-08-10 16:44:27 +0530

    Grid Basic Layout done...finishing touches remaining

--- templates/grid.php
+++ templates/grid.php
@@ -1,20 +1,65 @@
 <style>
     #container{
-        max-width: 97%;
         margin:10px auto;
+        max-width: 98%;
     }
+    #hotel{
+        margin-left: 2%;
+        margin-right: 2%;
+        margin-top:1%;
+        margin-bottom: 1%;
+        height: 200px;
+    }
+    #column-left{
+        margin: 0.75%;
+        height: 500px;
+    }
+    #column-right{
+        margin: 0.75%;
+        height: 500px;
+    }
+    #outer-row{
+        padding-top: 15px;
+    }
+    
 </style>
 
-
-
 <div class="container" id="container">
-  <h1>Hello World!</h1>
-  <div class="row">
-    <div class="col-sm-6" style="background-color:yellow;">
-      <p>Lorem ipsum...</p>
-    </div>
-    <div class="col-sm-6" style="background-color:pink;">
-      <p>Sed ut perspiciatis...</p>
-    </div>
-  </div>
+   <div class="row" id="outer-row">
+        <div class="col-sm-3" id = "column-left" style="background-color:yellow;">
+            <p>Lorem ipsum...</p>
+        </div>
+        <div class="col-sm-8" id = "column-right">
+            <h1>Top Rated!</h1>
+            <div class="row" id="inner-row">
+                <div class="col-sm-3" id = "hotel" style="background-color:yellow;">
+                    <p>Lorem ipsum...</p>
+                </div>
+                <div class="col-sm-3" id = "hotel" style="background-color:pink;">
+                    <p>Sed ut perspiciatis...</p>
+                </div>
+                <div class="col-sm-3" id = "hotel" style="background-color:yellow;">
+                    <p>Lorem ipsum...</p>
+                </div>
+            </div>
+            
+             <h1>Nearby!</h1>
+            <div class="row" id="inner-row">
+                <div class="col-sm-3" id = "hotel" style="background-color:yellow;">
+                    <p>Lorem ipsum...</p>
+                </div>
+                <div class="col-sm-3" id = "hotel" style="background-color:pink;">
+                    <p>Sed ut perspiciatis...</p>
+                </div>
+                <div class="col-sm-3" id = "hotel" style="background-color:yellow;">
+                    <p>Lorem ipsum...</p>
+                </div>
+            </div>
+        </div>
+        
 </div>
+    
+    
+    
+    
+  
