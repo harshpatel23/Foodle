de448796e04cc765da2b79fe95b842cc2e26f27a
Tanay Raul <tanay.raul@somaiya.edu>
2018-08-22 19:36:27 +0530

    login solved

--- login.php
+++ login.php
@@ -7,20 +7,34 @@
 	{
 		echo '<link rel="stylesheet" type="text/css" href="styles/login.css">';
 	}
+ 
+
  ?>
+			
 
 	<div class="form-group mx-auto text-center" style="width: 600px">
 	<form class="form-signin" id="login-form" method="post">
 		<img class="mx-auto d-block" src="images/Foodle.png" alt="logo" width="200" height=auto>
 		<h1 class="h3 mb-3 font-weight-normal">Please sign in to continue</h1>
 			<div class="form-group">
-			  <input type="text" class="form-control"  placeholder="Enter username" style="font-size:13px" name = "usrname" required autofocus>
+			  <input type="text" class="form-control"  placeholder="Enter username" style="font-size:13px" name="usrname" id="usrname" required autofocus
+			  <?php 
+ 				if(isset($_SESSION['error'])){
+					if($_SESSION['error']=='password'){
+						echo "value='";
+						echo $_SESSION['temp'];
+						echo "'";
+					}
+				}
+			   ?>
+			  >
 			</div>
 
 			<?php 
 			if(isset($_SESSION['error'])){
 				if($_SESSION['error']=='username')
 					echo '<p style="color: red">Invalid username!</p>';	
+
 			} ?>
 			
 			<div class="form-group">
@@ -36,8 +50,9 @@
 			<div class="checkbox">
 			  <label><input type="checkbox" value="" checked>Remember me</label>
 			</div>
-
 		  	<button type="submit" class="btn btn-primary btn-block" style="font-size:inherit; margin:auto;" formaction="loginchk.php"><span class="glyphicon glyphicon-off"></span> Login</button>
+
+		  	<p>New user? <a href="signup.php">Signup</a></p>
 		  </form>
 		  </div>
 
