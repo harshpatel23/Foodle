2683c8d859f2656f95465b58277418b9428bda53
Tanay Raul <tanay.raul@somaiya.edu>
2018-10-06 13:40:39 +0530

    nearby restaurants works

--- index.php
+++ index.php
@@ -5,6 +5,7 @@ include 'templates/header.php';
 function addcss(){
 	echo '<link rel="stylesheet" type="text/css" href="styles/index.css">';
     echo '<link rel="stylesheet" type="text/css" href="styles/rest_det.css">';
+	echo '<script src="scripts/location.js"></script>';
 }
 
 include 'templates/navbar.php';
