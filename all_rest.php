3db4419e211757d521c4162c1ea11943bfcfc450
Harsh Patel <harsh.patel4@somaiya.edu>
2018-10-06 15:15:46 +0530

    Added all_rest.php

---
+++ all_rest.php
@@ -0,0 +1,23 @@
+<?php 
+session_start();
+include 'templates/header.php';
+
+function addcss(){
+	echo '<link rel="stylesheet" type="text/css" href="styles/index.css">';
+    echo '<link rel="stylesheet" type="text/css" href="styles/rest_det.css">';
+}
+
+include 'templates/navbar.php';
+
+
+    
+
+
+
+
+
+
+
+
+include 'templates/footer.php';
+?>
