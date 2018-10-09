b7358d0424e2d5f67c629900d64a12a3287cca7e
nidhi021 <npd@somaiya.edu>
2018-10-09 15:06:00 +0530

    OTP

--- contact.php
+++ contact.php
@@ -9,27 +9,32 @@
 	}
  ?>
 			
+<script type=text/javascript>
+    function new_mobile(){
+	document.getElementById('contact').removeAttribute("readonly");
 
+    }
+</script>
 	<div class="form-group mx-auto text-center" style="width: 600px">
 	<form class="form-signin" id="contact-form" method="post">
 		<img class="mx-auto d-block" src="images/Foodle.png" alt="logo" width="200" height=auto>
 		<h1 class="h3 mb-3 font-weight-normal">Please provide your mobile number</h1>
 			<div class="form-group">
-			  <input type="text" class="form-control"  placeholder="Enter contact" style="font-size:13px" name="contact" id="contact" required
+			  <input type="text" class="form-control"  placeholder="Enter contact" style="font-size:13px;margin-bottom:10px;" name="contact" id="contact" required
                 <?php 
  				if(isset($_SESSION['otp'])){	
 				    echo "value='";
 				    echo $_SESSION['contact'];
-				    echo "'";
+				    echo "' readonly>";
 				}
 				else{
 					echo 'autofocus';
 				}
-			   ?>>
+			   ?>
 			</div>
 		<?php 
 		if(!isset($_SESSION['otp'])){
-			echo '<button type="submit" class="btn btn-primary btn-block" style="font-size:inherit; margin:auto;" formaction="send-otp.php"><span class="glyphicon glyphicon-off"></span> Send OTP</button>';
+			echo '<button type="submit" class="btn btn-primary" style="font-size:inherit; margin:auto;" formaction="send-otp.php"><span class="glyphicon glyphicon-off"></span> Send OTP</button>';
 		}
 		?>
 		<?php     
@@ -39,9 +44,11 @@
 					echo '<div class="form-group">
 			  <input type="text" class="form-control" name="otp" placeholder="Enter OTP" style="font-size:13px" required>
 			</div>
-
+            
 			<div>
-		  		<button type="submit" class="btn btn-primary btn-block" style="font-size:inherit; margin:auto;" formaction="verify_otp.php"><span class="glyphicon glyphicon-ok"></span>&nbsp;</span>Verify</button>
+            <button type="button" class="btn btn-primary" onclick="new_mobile()"style="font-size:inherit; margin-bottom:5px; margin-top:5px; width:130px">Change contact</button>
+                <button type="button" class="btn btn-primary" style="font-size:inherit; margin-bottom:5px; margin-top:5px; width:130px" formaction="send-otp.php"> Re-send OTP</button>
+		  		<button type="submit" class="btn btn-primary" style="font-size:inherit; margin-bottom:5px; margin-top:5px; width:130px" formaction="verify_otp.php"><span class="glyphicon glyphicon-ok"></span>&nbsp;</span>Verify</button>
 			</div>';
 				}
 
@@ -49,7 +56,7 @@
 			
 			<!--<div class="form-group">
 			  <input type="text" class="form-control" name="otp" placeholder="Enter OTP" style="font-size:13px" required>
-			</div>
+			</div    >
 
 			<div>
 		  		<button type="submit" class="btn btn-primary btn-block" style="font-size:inherit; margin:auto;" formaction="loginchk.php"><span class="glyphicon glyphicon-off"></span> Login</button>
