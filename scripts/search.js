f224140a44d7f3af4778d3f1ff2b736c30f26653
Harsh Patel <harsh.patel4@somaiya.edu>
2018-10-10 11:13:41 +0530

    Search variables changed

--- scripts/search.js
+++ scripts/search.js
@@ -1,27 +1,23 @@
 function search(str){
     if(str.length > 0){
-        alert(str);
         if(window.XMLHttpRequest){
             xhttp=new XMLHttpRequest();
         } 
         else{  
             xhttp=new ActiveXObject("Microsoft.XMLHTTP");
         }
-    
         xhttp.onreadystatechange = function(){
           if(this.readyState == 4 && this.status == 200){
-              alert(this.responseText);
                 var names = JSON.parse(this.responseText);
                 for(var i=1;i<=names.length;i++){
-                    document.getElementById(i).setAttribute("value",names[i]);
+                    document.getElementById(i).setAttribute("value",names[i-1]);
                 }
               for(;i<=5;i++){
                     document.getElementById(i).setAttribute("value","");
                 }
              }
-        };
-        alert("search_results.php?name="+str);
-        xmlhttp.open("GET","search_results.php?name="+str,true);
-        xmlhttp.send();
+            };
+        xhttp.open("GET","search_results.php?name="+str,true);
+        xhttp.send();
     }
 }
