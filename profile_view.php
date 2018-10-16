5564b3e0ebc383b0693987bb20f70d0fe0e4d347
Tanay Raul <tanay.raul@somaiya.edu>
2018-10-16 22:34:13 +0530

    db - rest-manager_id

--- profile_view.php
+++ profile_view.php
@@ -16,15 +16,21 @@ if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
 
-if($_SESSION['role'] == 'admin')
-	$user = $_GET['user_id'];
-else
-	$user = $_SESSION['uname'];
 
-if(isset($_GET['method']) && $_GET['method']=='insert')
+if(isset($_GET['method']) && $_GET['method']=='insert' && $_SESSION['role']=='admin')
     $flag = 'true';
 else
+{
     $flag = '';
+    if($_SESSION['role'] == 'admin')
+        $user = $_GET['user_id'];  
+    else
+        $user = $_SESSION['uname'];
+echo $flag;
+if($flag)
+    echo "string";
+
+
 $sql = "SELECT * from person where user_id = '$user'";
 
 
@@ -33,29 +39,29 @@ $result = mysqli_query($conn, $sql);
 $row = mysqli_fetch_assoc($result);
 
 mysqli_close($conn);
-
+}
 
 ?>
 
 <form id="profile" action="edit_profile.php"name="profile" method="post" style = "font-size: 15px; width: 500px; margin: 0 auto">
         <div class="form-group" style = "margin:10px 0px 10px; font-size: inherit;">
             <label for="fname">First Name:<span style="color : red"> * </span></label>
-            <input type="text" class="form-control" "margin:10px 0px 10px; font-size: inherit;" name="fname" id="fname" value="<?php if() echo $row ['fname']; ?>" <?php if($_SESSION['role'] != 'admin') echo "readonly" ?> required pattern="[a-zA-z]+$">
+            <input type="text" class="form-control" "margin:10px 0px 10px; font-size: inherit;" name="fname" id="fname" value="<?php if($flag) echo $row ['fname']; ?>" <?php if($_SESSION['role'] != 'admin') echo "readonly" ?> required pattern="[a-zA-z]+$">
             
             <label for="lname">Last Name:</label>
-            <input type="text" class="form-control" "margin:10px 0px 10px; font-size: inherit;" name="lname" id="lname" value="<?php echo $row ['lname']; ?>" <?php if($_SESSION['role'] != 'admin') echo "readonly" ?> pattern="[A-Za-z]+$">
+            <input type="text" class="form-control" "margin:10px 0px 10px; font-size: inherit;" name="lname" id="lname" value="<?php if($flag) echo $row ['lname']; ?>" <?php if($_SESSION['role'] != 'admin') echo "readonly" ?> pattern="[A-Za-z]+$">
             
             <label for="contact">Mobile no.: <span style="color : red"> * </span></label>
-            <input type="text" class="form-control" "margin:10px 0px 10px; font-size: inherit;" name="contact" id="contact" value="<?php echo $row ['contact']; ?>" <?php if($_SESSION['role'] != 'admin') echo "readonly" ?> required pattern="[0-9]{10}$">
+            <input type="text" class="form-control" "margin:10px 0px 10px; font-size: inherit;" name="contact" id="contact" value="<?php if($flag) echo $row ['contact']; ?>" <?php if($_SESSION['role'] != 'admin') echo "readonly" ?> required pattern="[0-9]{10}$">
             
             <label for="email">Email-Id:</label>
-            <input type="email" class="form-control" "margin:10px 0px 10px; font-size: inherit;" name="email" id="email" aria-describedby="emailHelp" value="<?php echo $row ['email']; ?>" <?php if($_SESSION['role'] != 'admin') echo "readonly" ?>>
+            <input type="email" class="form-control" "margin:10px 0px 10px; font-size: inherit;" name="email" id="email" aria-describedby="emailHelp" value="<?php  if($flag) echo $row ['email']; ?>" <?php if($_SESSION['role'] != 'admin') echo "readonly" ?>>
             
             <label for="fname">Username:<span style="color : red"> * </span></label>
-            <input type="text" class="form-control" "margin:10px 0px 10px; font-size: inherit;" name="uname" value="<?php echo $row ['user_id']; ?>" readonly>
+            <input type="text" class="form-control" "margin:10px 0px 10px; font-size: inherit;" name="uname" value="<?php if($flag) echo $row ['user_id']; ?>" readonly>
             
             <label for="pwd">Password<span style="color : red"> * </span></label>
-            <input type="text" class="form-control" "margin:10px 0px 10px; font-size: inherit;" name="pwd" id="pwd" value="<?php echo $row ['pwd']; ?>" <?php if($_SESSION['role'] != 'admin') echo "readonly" ?> required minlength="8">
+            <input type="text" class="form-control" "margin:10px 0px 10px; font-size: inherit;" name="pwd" id="pwd" value="<?php  if($flag) echo $row ['pwd']; ?>" <?php if($_SESSION['role'] != 'admin') echo "readonly" ?> required minlength="8">
 			
 			<?php if($_SESSION['role']=='admin')
 	echo 	'<label for="role">Role:<span style="color : red"> * </span></label>
