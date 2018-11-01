e64f1c36cae4421db812f8c53113965083488773
Tanay Raul <tanay.raul@somaiya.edu>
2018-11-01 11:11:26 +0530

    Minor bug fix

--- profile_view.php
+++ profile_view.php
@@ -44,7 +44,7 @@ mysqli_close($conn);
 ?>
 
 <form id="profile" action="edit_profile.php?method=<?php echo $method; ?>" name="profile" method="post" style = "font-size: 15px; width: 500px; margin: 0 auto">
-        <div class="form-group" style = style="margin:10px 0px 10px; font-size: inherit;">
+        <div class="form-group" style="margin:10px 0px 10px; font-size: inherit;">
             <label for="fname">First Name:<span style="color : red"> * </span></label>
             <input type="text" class="form-control" style="margin:10px 0px 10px; font-size: inherit;" name="fname" id="fname" value="<?php if($flag) echo $row ['fname']; ?>" <?php if($_SESSION['role'] != 'admin') echo "readonly" ?> required pattern="[a-zA-z]+$">
             
@@ -55,7 +55,7 @@ mysqli_close($conn);
             <input type="text" class="form-control" style="margin:10px 0px 10px; font-size: inherit;" name="contact" id="contact" value="<?php if($flag) echo $row ['contact']; ?>" <?php if($_SESSION['role'] != 'admin') echo "readonly" ?> required pattern="[0-9]{10}$">
             
             <label for="email">Email-Id:</label>
-            <input type="email" class="form-control" style="margin:10px 0px 10px; font-size: inherit;" name="email" id="email" aria-describedby="emailHelp" value="<?php  if($flag) echo $row ['email']; ?>" <?php if($_SESSION['role'] != 'admin') echo "readonly" ?>>
+            <input type="email" class="form-control" style="margin:10px 0px 10px; font-size: inherit;" name="email" id="email" value="<?php  if($flag) echo $row ['email']; ?>" <?php if($_SESSION['role'] != 'admin') echo "readonly" ?>>
             
             <label for="fname">Username:<span style="color : red"> * </span></label>
             &nbsp;<span id="correct" class="glyphicon glyphicon-ok" style="color:green;display:none;"></span>
